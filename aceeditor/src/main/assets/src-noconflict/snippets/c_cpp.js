982076d460c4dabc58bd2d245d4864d195497565
Azure <961523404@qq.com>
2019-12-22 00:31:20 +0800

    Update c_cpp.js

--- aceeditor/src/main/assets/src-noconflict/snippets/c_cpp.js
+++ aceeditor/src/main/assets/src-noconflict/snippets/c_cpp.js
@@ -2,54 +2,21 @@ ace.define("ace/snippets/c_cpp",["require","exports","module"], function(require
 "use strict";
 
 exports.snippetText = "## STL Collections\n\
-# array\n\
-snippet array\n\
-	array<${1:T}, ${2:N}> ${3};${4}\n\
 # vector\n\
 snippet vector\n\
 	vector<${1:T}> ${2};${3}\n\
-# deque\n\
-snippet deque\n\
-	deque<${1:T}> ${2};${3}\n\
-# forward_list\n\
-snippet flist\n\
-	forward_list<${1:T}> ${2};${3}\n\
-# list\n\
-snippet list\n\
-	list<${1:T}> ${2};${3}\n\
 # set\n\
 snippet set\n\
 	set<${1:T}> ${2};${3}\n\
 # map\n\
 snippet map\n\
 	map<${1:Key}, ${2:T}> ${3};${4}\n\
-# multiset\n\
-snippet mset\n\
-	multiset<${1:T}> ${2};${3}\n\
-# multimap\n\
-snippet mmap\n\
-	multimap<${1:Key}, ${2:T}> ${3};${4}\n\
-# unordered_set\n\
-snippet uset\n\
-	unordered_set<${1:T}> ${2};${3}\n\
-# unordered_map\n\
-snippet umap\n\
-	unordered_map<${1:Key}, ${2:T}> ${3};${4}\n\
-# unordered_multiset\n\
-snippet umset\n\
-	unordered_multiset<${1:T}> ${2};${3}\n\
-# unordered_multimap\n\
-snippet ummap\n\
-	unordered_multimap<${1:Key}, ${2:T}> ${3};${4}\n\
 # stack\n\
 snippet stack\n\
 	stack<${1:T}> ${2};${3}\n\
 # queue\n\
 snippet queue\n\
 	queue<${1:T}> ${2};${3}\n\
-# priority_queue\n\
-snippet pqueue\n\
-	priority_queue<${1:T}> ${2};${3}\n\
 ##\n\
 ## Access Modifiers\n\
 # private\n\
@@ -132,6 +99,18 @@ snippet lld\n\
 	[${1}](${2}){\n\
 		${3:/* code */}\n\
 	}${4}\n\
+# include\n\
+snippet include\n\
+	#include <${1}>\n\
+# using namespace std\n\
+snippet std\n\
+	using namespace std;\n\
+# iostream\n\
+snippet iostream\n\
+	#include <iostream>\n\
+# stdio\n\
+snippet stdio\n\
+	#include <stdio.h>\n\
 ";
 exports.scope = "c_cpp";
 
